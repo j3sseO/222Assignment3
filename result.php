d676024c0322a7dfd7065c472875b896e0c0ab6f
Jesse OConnor <jesseoconnor000@gmail.com>
2023-09-29 18:31:54 +1300

    Website functionality complete Just need to make it look nice

--- result.php
+++ result.php
@@ -17,6 +17,106 @@
         $patientFirstname = isset($_COOKIE["patient-firstname"]) ? $_COOKIE["patient-firstname"] : "";
 
         // Check if patient details exist in cookies
+        if (!empty($patientNHI) && !empty($patientSurname) && !empty($patientFirstname)) {
+            echo "<p>Patient Details:</p>";
+            echo "<p>NHI Number: $patientNHI</p>";
+            echo "<p>Surname: $patientSurname</p>";
+            echo "<p>First Name: $patientFirstname</p>";
+        } else {
+            echo "<p>Patient details not found.</p>";
+        }
+
+        $respiratory_numeric = $_POST["respiratory_numeric"];
+        $mechanical_ventilation = $_POST["mechanical_ventilation"];
+        $cardiovascular_numeric = $_POST["cardio_system"];
+        $nervoussystem_numeric = $_POST["nervous_system"];
+        $liver_numeric = $_POST["liver_system"];
+        $coagulation_numeric = $_POST["coagulation_numeric"];
+        $kidneys_numeric = $_POST["kidneys_system"];
+
+        $totalSOFA = 0;
+        
+        // Determine respitory score to add to total
+        if ($respiratory_numeric >= 400) {
+            $totalSOFA = $totalSOFA;
+        } else if ($respiratory_numeric >= 300) {
+            $totalSOFA = $totalSOFA + 1;
+        } else if ($respiratory_numeric >= 200) {
+            $totalSOFA = $totalSOFA + 2;
+        } else if ($respiratory_numeric >= 100 && $mechanical_ventilation == 1) {
+            $totalSOFA = $totalSOFA + 3;
+        } else if ($respiratory_numeric >= 0 && $mechanical_ventilation == 1) {
+            $totalSOFA = $totalSOFA + 4;
+        }
+
+        // Add selected cardiovascular system numeric to total
+        $totalSOFA = $totalSOFA + $cardiovascular_numeric;
+
+        // Determine central nervous system score to add to total
+        if ($nervoussystem_numeric == 15) {
+            $totalSOFA = $totalSOFA;
+        } else if ($nervoussystem_numeric >= 13) {
+            $totalSOFA = $totalSOFA + 1;
+        } else if ($nervoussystem_numeric >= 10) {
+            $totalSOFA = $totalSOFA + 2;
+        } else if ($nervoussystem_numeric >= 6) {
+            $totalSOFA = $totalSOFA + 3;
+        } else if ($nervoussystem_numeric < 6) {
+            $totalSOFA = $totalSOFA + 4;
+        }
+
+        // Determine liver score to add to total
+        if ($liver_numeric < 1.2) {
+            $totalSOFA = $totalSOFA;
+        } else if ($liver_numeric <= 1.9) {
+            $totalSOFA = $totalSOFA + 1;
+        } else if ($liver_numeric <= 5.9) {
+            $totalSOFA = $totalSOFA + 2;
+        } else if ($liver_numeric <= 11.9) {
+            $totalSOFA = $totalSOFA + 3;
+        } else if ($liver_numeric >= 12) {
+            $totalSOFA = $totalSOFA + 4;
+        }
+
+        // Determine coagulation score to add to total
+        if ($coagulation_numeric >= 150) {
+            $totalSOFA = $totalSOFA;
+        } else if ($coagulation_numeric > 100) {
+            $totalSOFA = $totalSOFA + 1;
+        } else if ($coagulation_numeric > 50) {
+            $totalSOFA = $totalSOFA + 2;
+        } else if ($coagulation_numeric > 20) {
+            $totalSOFA = $totalSOFA + 3;
+        } else if ($coagulation_numeric > 0) {
+            $totalSOFA = $totalSOFA + 4;
+        }
+
+        // Determine kidneys score to add to total
+        if ($kidneys_numeric < 1.2) {
+            $totalSOFA = $totalSOFA;
+        } else if ($kidneys_numeric <= 1.9) {
+            $totalSOFA = $totalSOFA + 1;
+        } else if ($kidneys_numeric <= 3.4) {
+            $totalSOFA = $totalSOFA + 2;
+        } else if ($kidneys_numeric <= 4.9) {
+            $totalSOFA = $totalSOFA + 3;
+        } else if ($kidneys_numeric >= 5) {
+            $totalSOFA = $totalSOFA + 4;
+        }
+
+        echo "<h2>SOFA Score Components:</h2>";
+        echo "<tr><th>Total SOFA Score: </th><th>$totalSOFA</th></tr>";
+
+        // echo "<table>";
+        // echo "<tr><th>Component</th><th>Value</th></tr>";
+        // echo "<tr><td>Respiratory Numeric</td><td>$respiratory_numeric</td></tr>";
+        // echo "<tr><td>Mechanical Ventilation</td><td>$mechanical_ventilation</td></tr>";
+        // echo "<tr><td>Cardiovascular Numeric</td><td>$cardiovascular_numeric</td></tr>";
+        // echo "<tr><td>Liver Numeric</td><td>$liver_numeric</td></tr>";
+        // echo "<tr><td>Coagulation Numeric</td><td>$coagulation_numeric</td></tr>";
+        // echo "<tr><td>Kidneys Numeric</td><td>$kidneys_numeric</td></tr>";
+        // echo "<tr><th>Total SOFA Score</th><th>$totalSOFA</th></tr>";
+        // echo "</table>";
         ?>
     </body>
 </html>
--- sofa.php
+++ sofa.php
@@ -33,26 +33,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <p>Surname: <?php echo $patientSurname; ?></p>
         <p>First Name: <?php echo $patientFirstname; ?></p>
 
-        <form action="results.php" method="POST">
+        <form action="result.php" method="POST">
             <h2>Enter SOFA Score Components</h2>
 
-            <label for="respitory_system">Respitory System:</label>
-            <input type="number" id="respitory_system" name="respitory_system" required><br><br>
+            <label for="respitory_numeric">Respitory Numeric (<span class="bold">PaO2/FiO2 [mmHg (kPa)]</span>):</label>
+            <input type="number" id="respiratory_numeric" name="respiratory_numeric" required><br><br>
 
-            <label for="nervous_system">Central Nervous System:</label>
+            <label>Mechanical Ventilation:</label>
+            <input type="radio" id="mechanical_ventilation_yes" name="mechanical_ventilation" value="1" required>
+            <label for="mechanical_ventilation_yes">Yes</label>
+            <input type="radio" id="mechanical_ventilation_no" name="mechanical_ventilation" value="0" required>
+            <label for="mechanical_ventilation_no">No</label><br><br>
+
+            <label for="nervous_system">Central Nervous System (<span class="bold">Glasgow Coma Scale</span>):</label>
             <input type="number" id="nervous_system" name="nervous_system" required><br><br>
 
-            <label for="cardio_system">Cardiovascular System:</label>
-            <input type="number" id="cardio_system" name="cardio_system" required><br><br>
+            <label for="cardio_system">Cardiovascular System (<span class="bold">Mean arterial pressure OR administration of vasopressors required</span>):</label><br><br>
+            <input type="radio" id="cardio_zero" name="cardio_system" value="0" required>
+            <label for="cardio_zero">MAP ≥ 70 mmHg</label><br><br>
+            <input type="radio" id="cardio_one" name="cardio_system" value="1" required>
+            <label for="cardio_one">MAP < 70 mmHg</label><br><br>
+            <input type="radio" id="cardio_two" name="cardio_system" value="2" required>
+            <label for="cardio_two">dopamine ≤ 5 μg/kg/min OR dobutamine (any dose)</label><br><br>
+            <input type="radio" id="cardio_three" name="cardio_system" value="3" required>
+            <label for="cardio_three">dopamine > 5 μg/kg/min OR epinephrine ≤ 0.1 μg/kg/min OR norepinephrine ≤ 0.1 μg/kg/min</label><br><br>
+            <input type="radio" id="cardio_four" name="cardio_system" value="4" required>
+            <label for="cardio_four">dopamine > 15 μg/kg/min OR epinephrine > 0.1 μg/kg/min OR norepinephrine > 0.1 μg/kg/min</label><br><br>
+            
 
-            <label for="liver_system">Liver Value:</label>
-            <input type="number" id="liver_system" name="liver_system" required><br><br>
+            <label for="liver_system">Liver Value (<span class="bold">Bilirubin (mg/dl) [μmol/L]</span>):</label>
+            <input type="number" step="0.1" id="liver_system" name="liver_system" required><br><br>
 
-            <label for="coagulation_system">Coagulation Value:</label>
-            <input type="number" id="respitory_system" name="respitory_system" required><br><br>
+            <label for="coagulation_system">Coagulation Value (<span class="bold">Platelets (×10^3/μl)</span>):</label>
+            <input type="number" id="coagulation_system" name="coagulation_numeric" required><br><br>
 
-            <label for="kidneys_system">Kidney Values:</label>
-            <input type="number" id="kidneys_system" name="kidneys_system" required><br><br>
+            <label for="kidneys_system">Kidney Values (<span class="bold">Creatinine (mg/dl) [μmol/L]</span>):</label>
+            <input type="number" step="0.1" id="kidneys_system" name="kidneys_system" required><br><br>
 
             <input type="submit" value="Calculate SOFA Score">
         </form>
